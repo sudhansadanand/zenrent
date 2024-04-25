b3d0de882f7a22d23eabcf32408b0315db0ffaff
Sudhan Sadanand <sudhansadanand@gmail.com>
2024-04-25 14:54:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -161,11 +161,11 @@ def main():
                         availability = str(entry["summary_Availability"])
                         if "N/A" in society_name:
                             popup_content = f"""
-                            <p><b>{title}</b></p><p>{bed_bath_sqft}</p><p>Availability: {availability} Rent: {rent}, Advance: {advance} </p><p>Id: {listing_id}</p><a href={url_link} target="_blank">Listing</a>
+                            <p><b>{title}</b></p><p>{bed_bath_sqft}</p><p><b>Availability: </b>{availability} <b>Rent: </b>{rent}, <b>Advance: </b>{advance} </p><p><b>Id: </b>{listing_id}</p><a href={url_link} target="_blank"><b>Listing</b></a>
                             """
                         else:
                             popup_content = f"""
-                            <p><b>{title}</b></p><p>Society Name: {society_name} </p><p>{bed_bath_sqft}</p><p>Availability: {availability} Rent: {rent}, Advance: {advance} </p><p>id: {listing_id}</p><a href={url_link} target="_blank">Listing</a>
+                            <p><b>{title}</b></p><p>Society Name: {society_name} </p><p>{bed_bath_sqft}</p><p><b>Availability: </b>{availability} <b>Rent: </b>{rent}, <b>Advance: </b>{advance} </p><p>id: {listing_id}</p><a href={url_link} target="_blank"><b>Listing</b></a>
                             """
                         folium.Marker(
                             location=[entry['geo_latitude'], entry['geo_longitude']],
